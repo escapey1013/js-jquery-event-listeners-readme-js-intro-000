f8a49c2c65a05d9cbc3fbb4ba2d15127223c2eea
Simon Kilroy <simonkilroy1@gmail.com>
2020-04-08 08:31:21 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,6 +1,22 @@
-//define functions here
+
+function getIt() {
+  $('p').on('click', function() {
+    alert.("Hey!");
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass("tasty")
+  })
+}
 
 $(document).ready(function(){
+
+getIt()
+
+frameIt()
+
 
 // call functions here
 

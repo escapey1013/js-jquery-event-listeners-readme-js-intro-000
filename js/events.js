ed45fe3c75959e35155dd9698ca7b5d32c83f09e
Simon Kilroy <simonkilroy1@gmail.com>
2020-04-08 09:06:50 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,8 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('#typing').on('keydown', function() {
+  $('#typing').on('keydown', function(e) {
+    if (e.which == 71)
     alert("You've pressed 'g'")
   })
 }
